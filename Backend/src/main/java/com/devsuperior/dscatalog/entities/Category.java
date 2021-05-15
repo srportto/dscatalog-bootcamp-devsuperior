c7e0c6771aa6d3e06b3de98bf1b3768f73a75998
srportto <ceeh1100@gmail.com>
2021-05-15 09:50:19 -0300

    add config h2 database

--- Backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ Backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,24 +1,26 @@
 package com.devsuperior.dscatalog.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
     private static final long serialVerionUID = 1L;
 
     @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
-
-    public Category() {
-    }
-
-    public Category(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 }
