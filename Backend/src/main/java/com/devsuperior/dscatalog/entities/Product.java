18b5570b38979600c923ef89038a5c2552cad537
srportto <ceeh1100@gmail.com>
2021-05-20 07:31:38 -0300

    add mapeamento jpa n to n

--- Backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
+++ Backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
@@ -1,2 +1,52 @@
-package com.devsuperior.dscatalog.entities;public class Product {
+package com.devsuperior.dscatalog.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_product")
+public class Product implements Serializable {
+    private static final long serialVerionUID = 1L;
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column (columnDefinition = "TEXT") //para indicar para o banco que preciso de um texto longo (muda de varchar p/ text no bd)
+    private String description;
+    private Double price;
+    private String imgUrl;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant date;
+
+    @ManyToMany
+    @JoinTable(name = "tb_product_category", //nome da tabela de relacionamento do manyToMany
+    joinColumns = @JoinColumn(name ="product_id" ),
+    inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    Set<Category> categories = new HashSet<>();
+
+    public Product(Long id, String name, String description, Double price, String imgUrl, Instant date) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgUrl = imgUrl;
+        this.date = date;
+    }
 }
